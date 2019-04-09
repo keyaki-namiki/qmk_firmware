15b9f5b61ac1e6659779315bfadf74a2e0662bca
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2019-04-09 23:18:10 +0900

    update keyboards/answer40/keymaps/default/keymap.c

--- keyboards/answer40/keymaps/default/keymap.c
+++ keyboards/answer40/keymaps/default/keymap.c
@@ -51,31 +51,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_DEL, KC_BSPC,\
       KC_LCTL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_ENT,\
           KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_SLSH, RAISE,\
-               KC_LGUI,  LOWER,         KC_SPC,           LOWER,   LT(_GAME_RAISE,KC_ENT),RAISE, KC_RALT\
+               KC_LGUI,  LOWER,         KC_SPC,           KC_SPC,LT(_GAME_RAISE,KC_ENT),RAISE, KC_RALT\
   ),
   [_LOWER] = LAYOUT( \
     KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11, KC_DEL,\
       _______,  XXXXXXX, KC_INS,  KC_DEL,  KC_BTN3,  KC_F12, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, XXXXXXX, XXXXXXX,\
           _______, XXXXXXX, KC_BSPC, XXXXXXX, KC_ENT,  KC_HOME, KC_PGDN, XXXXXXX, KC_PGUP, KC_END,  XXXXXXX, _______,\
-              _______,  LOWER,          XXXXXXX,          XXXXXXX,     XXXXXXX,          ADJUST, _______\
+              _______,  LOWER,          XXXXXXX,           XXXXXXX,     XXXXXXX,        ADJUST,_______\
   ),
   [_RAISE] = LAYOUT( \
     KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX, KC_GRV,\
       _______,  KC_4,    KC_5,    KC_6,    KC_DOT, XXXXXXX, KC_LBRC, KC_MINS, KC_EQL, KC_BSLS, KC_LPRN,   KC_QUOT,\
-          _______, KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC, KC_SLSH, S(KC_COMM),S(KC_DOT),S(KC_SLSH),KC_RPRN,_______,\
-              _______,  ADJUST,     MT(MOD_LSFT,KC_TAB),   KC_TAB,        XXXXXXX,      RAISE,  _______\
+          _______, KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC, KC_SLSH, XXXXXXX,S(KC_COMM),S(KC_DOT),KC_RPRN,_______,\
+              _______,  ADJUST,     MT(MOD_LSFT,KC_TAB),   KC_TAB,      XXXXXXX,        RAISE, _______\
   ),
   [_GAME_RAISE] = LAYOUT( \
-    KC_ESC,   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5, XXXXXXX, KC_GRV,\
-      _______,  KC_4,    KC_5,    KC_6,    KC_DOT, XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_QUOT,\
-          _______, KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC, KC_SLSH, S(KC_COMM),S(KC_DOT),KC_RSFT, KC_RPRN, _______,\
-              _______,  ADJUST,          KC_TAB,           KC_TAB,      XXXXXXX,        RAISE,  _______\
+    KC_ESC,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5, XXXXXXX, KC_GRV,\
+      _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_QUOT,\
+          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RBRC, KC_SLSH, XXXXXXX,S(KC_COMM),S(KC_DOT),KC_RPRN,_______,\
+              _______,  ADJUST,          KC_TAB,           KC_TAB,      XXXXXXX,        RAISE, _______\
   ),
   [_ADJUST] = LAYOUT( \
      RESET, DF(_QWERTY),XXXXXXX, XXXXXXX, RGBRST, RGB_TOG,    XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
       _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,DF(_GAME),RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,XXXXXXX, XXXXXXX,\
           _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUD, XXXXXXX, RGB_SAD, RGB_VAD, XXXXXXX, _______,\
-              _______,  LOWER,          XXXXXXX,          XXXXXXX,     XXXXXXX,         RAISE,  _______\
+              _______,  LOWER,          XXXXXXX,           XXXXXXX,     XXXXXXX,        RAISE, _______\
   ),
 };
 
