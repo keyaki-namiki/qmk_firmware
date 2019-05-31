0ae70f86325313b9ff493b3bf8518314d3678226
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2019-06-01 03:56:53 +0900

    update keyboards/answer40/keymaps/default/keymap.c

--- keyboards/answer40/keymaps/default/keymap.c
+++ keyboards/answer40/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
     KC_ESC,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_DEL, KC_BSPC,\
       KC_LCTL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_ENT,\
-          KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,\
+          KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,    KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_SLSH, LT(_RAISE,KC_SLSH),\
                KC_LGUI,  LOWER,MT(MOD_LSFT,KC_SPC),LT(_LOWER,KC_SPC),LT(_RAISE,KC_ENT),RAISE, KC_RALT\
   ),
   [_GAME] = LAYOUT( \
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_LOWER] = LAYOUT( \
     KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11, KC_DEL,\
-      _______,  XXXXXXX, KC_INS,  KC_DEL,  KC_BTN3,  KC_F12, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, XXXXXXX, XXXXXXX,\
-          _______, XXXXXXX, KC_BSPC, XXXXXXX, KC_ENT,  KC_HOME, KC_PGDN, XXXXXXX, KC_PGUP, KC_END,  XXXXXXX, _______,\
+      _______,  XXXXXXX, KC_INS,  KC_DEL,  KC_BTN3,  KC_F11, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, XXXXXXX, XXXXXXX,\
+          _______, XXXXXXX, KC_BSPC, XXXXXXX, KC_ENT, KC_F12,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, _______,\
               _______,  LOWER,          XXXXXXX,           XXXXXXX,     XXXXXXX,        ADJUST,_______\
   ),
   [_RAISE] = LAYOUT( \
