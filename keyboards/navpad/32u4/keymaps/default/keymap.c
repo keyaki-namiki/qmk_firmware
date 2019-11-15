7a0ec18c1c2000d5ed56ddac9f0cda62ce5d32e0
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2019-11-16 02:14:30 +0900

    edit default keymaps [WIP]

--- keyboards/navpad/32u4/keymaps/default/keymap.c
+++ keyboards/navpad/32u4/keymaps/default/keymap.c
@@ -20,20 +20,20 @@ enum custom_keycodes { QMKBEST = SAFE_RANGE, QMKURL };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] =LAYOUT (\
-	KC_PSCR, KC_SLCK, KC_PAUS,               KC_NO,   KC_NO,\
-	KC_INS,  KC_HOME, KC_PGUP, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,\
-	KC_DEL,  KC_END,  KC_PGDN, KC_P7, KC_P8, KC_P9,   KC_PPLS,\
-                               KC_P4, KC_P5, KC_P6,   KC_PPLS,\
-	KC_PGUP, KC_UP,   KC_PGDN, KC_P1, KC_P2, KC_P3,   LT(1,KC_PENT),\
-	KC_LEFT, KC_DOWN, KC_RGHT, KC_P0, KC_P0, KC_PDOT, LT(1,KC_PENT) \
+	KC_PSCR, KC_SLCK, KC_PAUS,                  KC_NO,   KC_NO,\
+	KC_INS,  KC_HOME, KC_PGUP, KC_NLCK,KC_PSLS, KC_PAST, KC_PMNS,\
+	KC_DEL,  KC_END,  KC_PGDN, KC_P7,  KC_P8,   KC_P9,   KC_PPLS,\
+                               KC_P4,  KC_P5,   KC_P6,   KC_PPLS,\
+	KC_PGUP, KC_UP,   KC_PGDN, KC_P1,  KC_P2,   KC_P3,   LT(1,KC_PENT),\
+	KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,  KC_P0,   KC_PDOT, LT(1,KC_PENT) \
     ),
     [1] =LAYOUT (\
-	KC_NO, RESET, KC_NO,               KC_NO,   KC_NO,\
-	KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,\
-	KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,\
-                               KC_NO, KC_NO, KC_NO,   KC_NO,\
-	KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,\
-	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
+	KC_NO,  KC_NO,  RESET,                KC_NO, KC_NO,\
+	KC_NO,  KC_NO,  KC_NO, BL_STEP,KC_NO, KC_NO, KC_NO,\
+	KC_NO,  KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,\
+                           KC_NO,  KC_NO, KC_NO, KC_NO,\
+	KC_NO, KC_NO,   KC_NO, KC_NO,  KC_NO, KC_NO, KC_TRNS,\
+	KC_NO, KC_NO,   KC_NO, KC_NO,  KC_NO, KC_NO, KC_TRNS \
     )
 };
 
