d42952a92618b8ff77b649c6cdad3e775752433b
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2020-01-07 00:20:46 +0900

    small change in keymap.c

--- keyboards/proto_split/keymaps/default/keymap.c
+++ keyboards/proto_split/keymaps/default/keymap.c
@@ -51,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
   [_LOWER] = LAYOUT( \
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_DEL,\
-    _______, XXXXXXX, KC_INS,  KC_DEL,  KC_BTN3,  KC_F11, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, XXXXXXX, XXXXXXX,\
-    _______, XXXXXXX, KC_BSPC, XXXXXXX, KC_ENT, KC_F12,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,\
+    _______, KC_BTN1, KC_INS,  KC_DEL,  KC_BTN3,  KC_F11, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, XXXXXXX, XXXXXXX,\
+    _______, KC_BTN2, KC_BSPC, KC_APP,  KC_ENT, KC_F12,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,\
          KC_NO,   KC_NO,   LOWER,   _______, XXXXXXX,          KC_TAB, _______,RAISE,   KC_NO,   KC_NO\
    ),
   [_RAISE] = LAYOUT( \
