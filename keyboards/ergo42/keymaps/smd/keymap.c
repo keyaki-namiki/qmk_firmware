827b9930d6902572778d6de609519eb4f1f79b70
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2020-01-28 20:29:06 +0900

    small modification in keymap.c

--- keyboards/ergo42/keymaps/smd/keymap.c
+++ keyboards/ergo42/keymaps/smd/keymap.c
@@ -35,12 +35,13 @@ enum custom_keycodes {
     ADJUST
 };
 #define KC_TCTL MT(MOD_LCTL,KC_TAB)
+#define KC_SSLS MT(MOD_RSFT,KC_SLSH)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(\
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_HOME, KC_END,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,\
     KC_TCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_INS,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,\
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,  KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ADJUST,\
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,  KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SSLS, ADJUST,\
     KC_NO,   KC_NO,   KC_NO,   LOWER,   KC_LGUI, KC_LSPC, KC_LSPC, KC_RENT, KC_RENT, KC_RALT, RAISE,   KC_NO,   KC_NO,   KC_NO \
 ),
     [_GAME] = LAYOUT(\
