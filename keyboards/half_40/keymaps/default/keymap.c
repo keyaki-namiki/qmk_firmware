f585bb5542da13ff7d70e705b3c9aec7ba8607c4
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2020-01-29 02:05:24 +0900

    started project half_40

---
+++ keyboards/half_40/keymaps/default/keymap.c
@@ -0,0 +1,153 @@
+/* Copyright 2020 keyaki-namiki
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _FLIP,
+    _NUM,
+    _CTRL,
+    _ADJUST
+};
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes { 
+    BASE = SAFE_RANGE,
+    KC_FSPC,
+    CONTROL,
+    LOWER,
+    KC_LSPC,
+    RAISE,
+    G_RAISE,
+    KC_RENT,
+    ADJUST
+};
+#define KC_TCTL MT(MOD_LCTL,KC_TAB)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT(
+        KC_ESC,  KC_Q,   KC_W,  KC_E,   KC_R,   KC_T,   KC_BSPC,\
+        KC_TCTL, KC_A,   KC_S,  KC_D,   KC_F,   KC_G,   KC_ENT,\
+        KC_LSFT, KC_Z,   KC_X,  KC_C,   KC_V,   KC_B,   KC_LALT,\
+                                                    KC_FSPC 
+    ),\
+    [_FLIP] = LAYOUT(
+        KC_ESC,  KC_P,   KC_O,  KC_I,   KC_U,   KC_Y,   CONTROL,\
+        KC_TCTL, KC_SCLN,KC_L,  KC_K,   KC_J,   KC_H,   KC_QUOT,\
+        KC_LSFT, KC_SLSH,KC_DOT,KC_COMM,KC_M,   KC_N,   KC_LALT,\
+                                                    KC_TRNS
+    )
+};
+
+void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
+	if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
+		layer_on(layer3);
+	} else {
+		layer_off(layer3);
+	}
+}
+
+static bool lower_pressed = false;
+static bool raise_pressed = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LOWER:
+            if (record->event.pressed) {
+                layer_on(_LOWER);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_LOWER);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+        case KC_FSPC:
+            if (record->event.pressed) {
+                lower_pressed = 1;
+
+                layer_on(_LOWER);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_LOWER);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+
+                if(lower_pressed){
+                    register_code(KC_SPC);
+                    unregister_code(KC_SPC);
+                }
+                lower_pressed = 0;
+            }
+            return false;
+        case RAISE:
+            if (record->event.pressed) {
+                layer_on(_RAISE);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_RAISE);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+        case KC_RENT:
+            if (record->event.pressed) {
+                raise_pressed = 1;
+
+                layer_on(_RAISE);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_RAISE);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+                
+                if(raise_pressed){
+                    register_code(KC_ENT);
+                    unregister_code(KC_ENT);
+                }
+                raise_pressed = 0;
+            }
+            return false;
+        case ADJUST:
+            if (record->event.pressed) {
+                layer_on(_ADJUST);
+            } else {
+                layer_off(_ADJUST);
+            }
+            return false;
+        default:
+            if (record->event.pressed) {
+                lower_pressed = 0;
+                raise_pressed = 0;
+            }
+            break;
+            
+    }
+
+    return true;
+}
+
+/*
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+bool led_update_user(led_t led_state) {
+    return true;
+}
+*/
