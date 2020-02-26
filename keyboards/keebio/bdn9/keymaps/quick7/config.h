13de71d62ad9c8fa1c7adfc9c6257d6eb001960e
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2020-02-26 18:20:02 +0900

    added keymaps and configs for quick7

---
+++ keyboards/keebio/bdn9/keymaps/quick7/config.h
@@ -0,0 +1,49 @@
+/*
+Copyright 2019 Danny Nguyen <danny@keeb.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define UNDERGLOW
+
+#undef BACKLIGHT_PIN 
+
+#undef RGB_DI_PIN
+#undef RGBLED_NUM
+
+#define RGB_DI_PIN D3
+#ifdef RGB_DI_PIN
+  #ifdef UNDERGLOW
+    #define RGBLED_NUM 13
+  #else
+    #define RGBLED_NUM 4
+  #endif
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+  #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+  /*== all animations enable ==*/
+  #define RGBLIGHT_ANIMATIONS
+#endif
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
