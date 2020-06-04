df8550a180d90b16430309a174416db2b0dd86c2
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2020-06-04 16:58:44 +0900

    update my keymaps

--- keyboards/ai03/equinox/keymaps/keyaki-namiki/keymap.c
+++ keyboards/ai03/equinox/keymaps/keyaki-namiki/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( /* Base */
     KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,\
     KC_TCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,\
-    KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSLS,\
+    KC_LSFT, KC_BSPC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSLS,\
     LOWER, XXXXXXX, KC_LGUI,          XXXXXXX,        KC_LSPC,       XXXXXXX,      KC_LALT, XXXXXXX, RAISE\
   ),
   [_LOWER] = LAYOUT( /* Extra Keys */
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT( /* Num */
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,\
     _______, KC_4,    KC_5,    KC_6,    KC_DOT,  XXXXXXX, KC_LBRC, KC_MINS, KC_EQL,  KC_BSLS, KC_LPRN, KC_QUOT,\
-    _______, KC_GRV,  KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX, KC_RBRC, KC_SLSH, XXXXXXX, XXXXXXX, KC_RSFT,\
+    _______, KC_GRV,  KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX, KC_RBRC, KC_SCLN, XXXXXXX, XXXXXXX, KC_RSFT,\
     LOWER, XXXXXXX, _______,          XXXXXXX,         KC_ATAB,         XXXXXXX,     _______, _______, RAISE\
   ),
   [_ADJUST] = LAYOUT( \
